a96e483e23208f0590554d3965ffb61c43c71fb1
Daniel Lindegren <onosendi@protonmail.com>
2020-05-25 16:14:40 -0700

    flask_boilerplate: fix import ordering

--- flask_boilerplate.py
+++ flask_boilerplate.py
@@ -1,9 +1,9 @@
 import os
-import config
 
 from app.app_factory import create_app
 from app.extensions import db
 from app.users.models import User
+import config
 
 if os.environ['FLASK_ENV'] == 'production':
     app = create_app(config.ProductionConfig)
