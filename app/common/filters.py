56fec55a10b07e940633744c8889830c8d1ece3c
Daniel Lindegren <onosendi@protonmail.com>
2020-07-17 19:04:03 -0700

    filters.py: comments

--- app/common/filters.py
+++ app/common/filters.py
@@ -1,6 +1,9 @@
 def truncate(text: str, max_length: int) -> str:
     ''' Truncate text if greater than `max_length`.
 
+    This function exists as an example on how to configure jinja filters. It is
+    not used anywhere in this boilerplate.
+
     :param text: Text to truncate
     :param max_length: Max length of text before truncating
     '''
