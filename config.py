4e4734e2ac416c5ef6a82b2b36b2458de0463091
Daniel Lindegren <onosendi@dlindegren.com>
2021-05-07 09:05:04 -0700

    Set SESSION_COOKIE_SAMESITE to 'lax'

--- config.py
+++ config.py
@@ -13,10 +13,12 @@ class BaseConfig:
     LOGIN_REDIRECT_ENDPOINT = 'users.index';
     DEBUG = False
     SECRET_KEY = os.environ['SECRET_KEY']
+
+    SESSION_COOKIE_SAMESITE = 'lax'
+
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') \
         or 'sqlite:///' + os.path.join(basedir, 'flask_boilerplate.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
     # If set to True SQLAlchemy will log all the statements issued to stderr
     # which can be useful for debugging.
     SQLALCHEMY_ECHO = False
